59c6a6549770c2869c29e7ed73e6bf49b8fe1a43
Arzu <119745513+arzunuzx1@users.noreply.github.com>
2023-01-19 18:54:03 +0300

    Update evenOdd.go

--- evenOdd.go
+++ evenOdd.go
@@ -4,35 +4,35 @@ import "fmt"
 
 func main() {
 
-	//Öğrencinin vize ve final notunu al
-	var vizeNotu float32
-	var finalNotu float32
+	//Get the student's midterm and final grade
+	var midtermGrade float32
+	var finalGrade float32
 
-	fmt.Println("Lütfen vize notunu giriniz:")
-	fmt.Scanln(&vizeNotu)
-	fmt.Println("Lütfen final notunu giriniz")
-	fmt.Scanln(&finalNotu)
+	fmt.Println("Please enter the midterm grade:")
+	fmt.Scanln(&midtermGrade)
+	fmt.Println("Please enter the final grade:")
+	fmt.Scanln(&finalGrade)
 
-	//Vize ve final notunu topla ortalamasını al
-	notTopla := vizeNotu + finalNotu
-	ortalama := notTopla / 2
+	//Add up midterm and final grades and take the average
+	noteTotal := midtermGrade + finalGrade
+	average := noteTotal / 2
 
-	//Ortalama notu ekrana yazdır
-	fmt.Println("Ortalama:", ortalama)
+	//Print average grade to screen
+	fmt.Println("Average:", average)
 
-	//Başka bir hesaplama yapmak isteyip istemediğini sor
-	fmt.Println("Başka bir hesap yapmak ister misiniz? (evet/hayır)")
-	var cevap string
-	fmt.Scanln(&cevap)
+	//Ask if would like to make another calculation
+	fmt.Println("Do you want to make another calculation?(Yes/No)")
+	var answer string
+	fmt.Scanln(&answer)
 
-	//Kullanıcı başka bir hesaplama yapmak isterse başa dön
+	//If the user wants to perform another calculation, go back to the beginning
 
-	if cevap == "evet" {
+	if answer == "Yes" {
 
-		//Kullanıcı başka bir hesaplama yapmak istemiyorsa bitir
+		//Finish if the user does not want to perform another calculation
 
-	} else if cevap == "hayır" {
-		fmt.Println("İşlem tamamlandı. Teşekkür ederiz.")
+	} else if answer == "No" {
+		fmt.Println("The transaction is complete. Thank you.")
 
 	}
 }
